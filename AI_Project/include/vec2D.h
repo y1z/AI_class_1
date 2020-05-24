ed6dd6e34ad51b3248d3eb12e7782f05aad04411
YB <whyis2cool@gmail.com>
2020-05-23 19:56:07 -0500

    Start making the 2D vector class.

---
+++ AI_Project/include/vec2D.h
@@ -0,0 +1,20 @@
+#pragma once
+
+class vec2D
+{
+public: // constructors
+  vec2D();
+  vec2D(float const & inputX, float const & inputY);
+  vec2D(vec2D const& other) = default;
+  vec2D(vec2D && other) = default;
+
+public:
+
+public:
+
+
+public:
+  float x;
+  float y;
+};
+
---
+++ AI_Project/src/vec2D.cpp
@@ -0,0 +1,11 @@
+#include "vec2D.h"
+
+vec2D::vec2D()
+  :x(0.0f),
+  y(0.0f)
+{}
+
+vec2D::vec2D(float const & inputX,float const &inputY)
+  : x(inputX),
+  y(inputY)
+{}
