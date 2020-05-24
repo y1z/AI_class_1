bfcd3bee722c3c55fcff814409b20a2ef05330ff
YB <whyis2cool@gmail.com>
2020-05-23 23:24:13 -0500

    vec2D now has 'add' and 'subtract' functions.

--- AI_Project/include/vec2D.h
+++ AI_Project/include/vec2D.h
@@ -1,17 +1,114 @@
 #pragma once
 
-class vec2D
+/**
+* @brief This class represent a 2d vector
+* @bug
+*/
+class vec2
 {
 public: // constructors
-  vec2D();
-  vec2D(float const & inputX, float const & inputY);
-  vec2D(vec2D const& other) = default;
-  vec2D(vec2D && other) = default;
+  vec2();
+  vec2(float const& X, float const& Y);
+  vec2(vec2 const& other) = default;
+  vec2(vec2&& other) noexcept = default;
 
 public:
 
+  /**
+  * @brief the same as the 'add' function.
+  * @param[in] other : the other vector that going to be in the sum.
+  */
+  vec2 
+  operator+(vec2 const& other)const;
+
+  /**
+  * @brief the same as the 'addToSelf' function.
+  * @param[in] other : the vector that going to be added to the current vector.
+  */
+  void
+  operator+=(vec2 const& other);
+
+  /**
+  * @brief the same as the 'subtract' function.
+  * @param[in] other : the other vector that going to be in the sum.
+  */
+  vec2 
+  operator-(vec2 const& other)const;
+
+  /**
+  * @brief the same as the 'subtractFromSelf' function.
+  * @param[in] other : the vector that going to be added to the current vector.
+  */
+  void
+  operator-=(vec2 const& other);
+
 public:
 
+  /**
+  * @returns the sum of the current vector and the input vector.
+  * @bug no known bugs.
+  * @param[in] other : the other vector to be added.
+  */
+  vec2 
+  add(vec2 const& other)const;
+
+  /**
+  * @returns the sum of the current vector and the input vector.
+  * @bug no known bugs.
+  * @param[in] other : the other vector to be added.
+  */
+  vec2 
+  add(float const& x, float const& y)const;
+
+  /**
+  * @brief Adds the input vector to the current one.
+  * @bug no known bugs.
+  * @param[in] other : the vector that going to be added.
+  */
+  void
+  addToSelf(vec2 const& other);
+
+  /**
+  * @brief Adds the input vector to the current one.
+  * @bug no known bugs.
+  * @param[in] x : the x value being added to the current vector.
+  * @param[in] y : the y value being added to the current vector.
+  */
+  void
+  addToSelf(float const& x, float const& y);
+
+  /**
+  * @returns The current vector minus the input vector.
+  * @bug no known bugs.
+  * @param[in] other : The to be subtracted by.
+  */
+  vec2 
+  subtract(vec2 const& other)const;
+
+  /**
+  * @returns The sum of the current vector and the input vector.
+  * @bug no known bugs.
+  * @param[in] other : the other vector to be added.
+  */
+  vec2 
+  subtract(float const& x, float const& y)const;
+
+  /**
+  * @brief Adds the input vector to the current one.
+  * @bug no known bugs.
+  * @param[in] other : The vector thats going to be subtracted from the current one.
+  */
+  void
+  subtractFromSelf(vec2 const& other);
+
+  /**
+  * @brief Subtracts the input vector from the current one.
+  * @bug no known bugs.
+  * @param[in] x : The x value being subtracted from the current vector.
+  * @param[in] y : The y value being subtracted from the current vector.
+  */
+  void
+  subtractFromSelf(float const& x, float const& y);
 
 public:
   float x;
--- AI_Project/src/vec2D.cpp
+++ AI_Project/src/vec2D.cpp
@@ -1,11 +1,90 @@
 #include "vec2D.h"
 
-vec2D::vec2D()
+vec2::vec2() 
   :x(0.0f),
   y(0.0f)
 {}
 
-vec2D::vec2D(float const & inputX,float const &inputY)
-  : x(inputX),
-  y(inputY)
+vec2::vec2(float const& X,
+             float const& Y)
+  : x(X),
+  y(Y)
 {}
+
+vec2 
+vec2::operator+(vec2 const& other) const
+{
+  return add(other);
+}
+
+void 
+vec2::operator+=(vec2 const& other)
+{
+  addToSelf(other);
+}
+
+vec2 
+vec2::operator-(vec2 const& other) const
+{
+  return subtract(other);
+}
+
+void 
+vec2::operator-=(vec2 const& other)
+{
+  subtractFromSelf(other);
+}
+
+vec2 
+vec2::add(vec2 const& other) const
+{
+  return vec2(this->x + other.x, this->y + other.y);
+}
+
+vec2 
+vec2::add(float const& x, float const& y) const
+{
+  return vec2(this->x + x, this->y + y);
+}
+
+void 
+vec2::addToSelf(vec2 const& other)
+{
+  this->x += other.x;
+  this->y += other.y;
+}
+
+void 
+vec2::addToSelf(float const& x, float const& y)
+{
+  this->x += x;
+  this->y += y;
+}
+
+vec2 
+vec2::subtract(vec2 const& other) const
+{
+  return vec2(this->x - other.x, this->y - other.y);
+}
+
+vec2 
+vec2::subtract(float const& x, float const& y) const
+{
+  return vec2(this->x - x, this->y - y);
+}
+
+void 
+vec2::subtractFromSelf(vec2 const& other)
+{
+  this->x -= other.x;
+  this->y -= other.y;
+}
+
+void 
+vec2::subtractFromSelf(float const& x, float const& y)
+{
+  this->x = x;
+  this->y = y;
+}
+
+
