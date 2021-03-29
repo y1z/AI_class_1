a89fa83a07f4d822c36e13972faac0e8320c8d0c
y1z <whyis2cool@gmail.com>
2021-03-29 01:01:57 -0600

    Move .cpp from "include" folder to "src" folder.

--- AI_Project/src/WindowConversion.cpp
+++ AI_Project/src/WindowConversion.cpp
@@ -1,10 +1,11 @@
 #include "WindowConversion.h"
 #include <cassert>
+#include <cmath>
 
 sf::Vector2u
 WindowConversion::getPositionFromPercentage(const sf::Vector2u& screenSize,
                                             const sf::Vector2f& percentage) {
-  assert(percentage.x <= 0.0f, percentage.y <= 0.f);
+  assert(!std::signbit(percentage.x) && !std::signbit(percentage.y));
   const float positionInX = percentage.x * screenSize.x;
   const float positionInY = percentage.y * screenSize.y;
   return sf::Vector2u(static_cast<unsigned int>(positionInX),
